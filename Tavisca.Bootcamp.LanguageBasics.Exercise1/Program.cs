100576055f864687968ae367240807ca6700c409
tavisca-akhullar <akhullar@tavisca.in>
2019-06-22 18:24:30 +0530

    Done with language-bsics-01

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,50 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            double divisor, dividend;
+            double result = -1;
+            String resultWithQues = "-1";
+            string[] splitString = equation.Split('=');
+            if (splitString[0].Contains("?"))
+            {
+                string[] splitPart1 = splitString[0].Split('*');
+
+                if (splitPart1[0].Contains("?"))
+                {
+                    divisor = int.Parse(splitPart1[1]);
+                    dividend = int.Parse(splitString[1]);
+                    resultWithQues = splitPart1[0];
+                    result = dividend / divisor;
+                }
+                else
+                {
+                    divisor = int.Parse(splitPart1[0]);
+                    dividend = int.Parse(splitString[1]);
+                    resultWithQues = splitPart1[1];
+                    result = dividend / divisor;
+                }
+            }
+            else
+            {
+                string[] splitPart2 = splitString[0].Split('*');
+                divisor = int.Parse(splitPart2[0]);
+                dividend = int.Parse(splitPart2[1]);
+                result = divisor * dividend;
+                resultWithQues = splitString[1];
+            }
+
+            char[] finalResult = resultWithQues.ToCharArray();
+            char[] number = result.ToString().ToCharArray();
+            if (result % 1 != 0 || finalResult.Length != number.Length)
+            {
+                return -1;
+            }
+            else
+            {
+                int index = Array.IndexOf(finalResult, '?');
+                return int.Parse(number[index] + "");
+            }
+
         }
     }
 }
